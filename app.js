45779cdef7265062fab967a92019dd24f9f330fc
nithesh407 <nitheshravikumar13631@gmail.com>
2024-02-02 14:41:04 +0530

    added vercel.json

--- app.js
+++ app.js
@@ -16,9 +16,10 @@ app.use(
   })
 );
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "*")
-// })
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*")
+  next()
+})
 
 app.get('/welcome', (req, res) => {
   res.status(200).json({
